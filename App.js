d16573376a5e7627ba2f580560c810bc8b5c7d99
jecodeg <132217853+jecodeg@users.noreply.github.com>
2023-07-08 19:33:51 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,78 +1,78 @@
-import { useState } from 'react';
-import {nanoid} from 'nanoid';
-import NotesList from './components/NotesList';
-import Header from './components/header';
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from'./components/NotesList';
+import Header from './components/header'
 
-const App =() =>{
-  const [notes, setNotes] =useState([
+const App = () => {
+  const [notes, setNotes] = useState([
     {
-    id:nanoid(),
-    text:"This is my first note!",
-    date: "27/11/2022"
-  },
-  {
-    id:nanoid(),
-    text:"This is my second note!",
-    date: "7/4/2023"
-  },
-  {
-    id:nanoid(),
-    text:"This is my third note!",
-    date: "4/7/2023"
-  },
-  {
-    id:nanoid(),
-    text:"This is my fourth note!",
-    date: "7/7/2023"
-  },
-  {
-    id:nanoid(),
-    text:"This is my fifth note!",
-    date: "7/7/2023"
-  }
-]);
-
-const [darkMode, setdarkMode]= useState(false);
-
-const addNote =(text)=>{
-  const date=new Date();
-  const newNote={
-    id:nanoid(),
-    text: text,
-    date: date.toLocaleDateString()
-  }
-  const newNotes=[...notes,newNote];
-  setNotes(newNotes);
-}
-
-const deleteNote =(id) =>{
-  const newNotes = notes.filter((note)=>note.id !== id);
-  setNotes(newNotes);
-}
-
-
-const handleUpdate = (id, newText) => {
-  const updatedNotes = notes.map((note) => {
-    if (note.id === id) {
-      return { ...note, text: newText };
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "27/11/2022"
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "7/4/2023"
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "4/7/2023"
+    },
+    {
+      id: nanoid(),
+      text: "This is my fourth note!",
+      date: "7/7/2023"
+    },
+    {
+      id: nanoid(),
+      text: "This is my fifth note!",
+      date: "7/7/2023"
     }
-    return note;
-  });
-  setNotes(updatedNotes);
-};
+  ]);
 
-  return( 
-  <div className={`${darkMode && 'dark-mode'}`}>
-          <div className="container">
-            <Header handleToggleDarkMode={setdarkMode}/>
-            <NotesList 
-            notes={notes} 
-            handleAddNote={addNote}
-            handleUpdate={handleUpdate}
-            handleDeleteNote={deleteNote}
-            />
-          </div>
-  </div>
+  const [darkMode, setDarkMode] = useState(false);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
+  const handleUpdate = (id, newText) => {
+    const updatedNotes = notes.map((note) => {
+      if (note.id === id) {
+        return { ...note, text: newText };
+      }
+      return note;
+    });
+    setNotes(updatedNotes);
+  };
+
+  return (
+    <div className={`app ${darkMode ? 'dark-mode' : ''}`}>
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode} />
+        <NotesList
+          notes={notes}
+          handleAddNote={addNote}
+          handleUpdate={handleUpdate}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
+    </div>
   );
 };
+
 export default App;
